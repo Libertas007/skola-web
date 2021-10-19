ed4a587f231d9dd388caece4c3d1a4ba08eb0067
Libertas <72643110+Libertas007@users.noreply.github.com>
2021-10-19 08:20:28 +0200

    hodnocení

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,10 @@ main()
 function main() {
     
 }
+
+document.getElementById("submit").onsubmit = (e) => {
+    e.preventDefault()
+    const value = document.getElementById("hodnoceni").value
+
+    
+}
