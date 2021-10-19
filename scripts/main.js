ec6bff26f448e6b7853e5b85d3734cbc7ea46f65
Libertas <72643110+Libertas007@users.noreply.github.com>
2021-10-19 08:30:58 +0200

    úprava stylů

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,9 @@ function main() {
 
 document.getElementById("submit").onsubmit = (e) => {
     e.preventDefault()
+    console.log("Zpracovávám údaje...")
     setTimeout(() => {
+        console.log("Hotovo :)")
         window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     }, 2000);
 }
