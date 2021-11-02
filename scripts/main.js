d066c57e47c4c6a967a4a640650a6beb07991001
Libertas <72643110+Libertas007@users.noreply.github.com>
2021-11-02 08:04:12 +0100

    oprava chyby

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,6 @@ document.getElementById("hodnoceni").oninput = (e) => {
 document.getElementById("submit").onclick = (e) => {
     e.preventDefault()
     console.log("Zpracovávám údaje...")
-    document.getElementById("notification").innerText = "Děkujeme za hodnocení!"
-    document.getElementById("notification").classList.remove("non-visible")
     setTimeout(() => {
         console.log("Hotovo :)")
         window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
